7f07d4ca6231cb856f39a6331590e4646fbc6d86
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-09-11 19:51:59 -0400

    initial

---
+++ src/components/DoctorScene/DoctorScene.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorScene = () => {
+    return (
+        <>
+            <img src="./assets/doctor.png" />
+        </>
+    )
+};
+
+export default DoctorScene;
